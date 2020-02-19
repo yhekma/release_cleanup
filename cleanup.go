96077d4996bde9e952051fc1a22a06bb0d63adc4
Yoram Hekma <yoram.hekma@tnt-digital.com>
2020-02-19 13:24:40 +0100

    add comment for helm --all

--- cleanup.go
+++ cleanup.go
@@ -116,6 +116,7 @@ func GetKubeOutput(namespace string) []byte {
 }
 
 func GetHelmOutput() []byte {
+	// Getting helm output on helm 2.13.1 with --all gives less output than without --all for some reason.....
 	cmd := exec.Command("helm", "list")
 	result := getOutput(cmd)
 	return result
